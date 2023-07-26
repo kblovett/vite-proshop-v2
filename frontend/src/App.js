2f43591a790494fc272eb38c1b14f398f1595532
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2023-07-26 17:42:08 -0600

    break time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
+import { Container } from 'react-bootstrap';
+
+import { Header } from './components';
+
 const App = () => {
-  return <h1>Welcome to Dimec</h1>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Dimec</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
