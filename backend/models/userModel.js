70bd86e0bcc0b10fcdfe5c14bf2dfa68825cfead
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2023-07-31 12:18:29 -0600

    register controller

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -11,19 +11,20 @@ const userSchema = new mongoose.Schema(
     },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, required: true, default: false },
-    // isSeller: { type: Boolean, required: true, default: false },
-    // seller: {
-    //   name: String,
-    //   logo: String,
-    //   description: String,
-    //   rating: { type: Number, default: 0, required: true },
-    //   numReviews: { type: Number, default: 0, required: true },
-    // },
   },
   {
     timestamps: true,
   }
 );
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
